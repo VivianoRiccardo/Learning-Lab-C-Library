f03c3e247d8b9f37d5a4ee77bab9348dcd277018
Riccardo Viviano <40870708+VivianoRiccardo@users.noreply.github.com>
2019-07-26 15:19:07 +0200

    Update math_functions.c

--- llab/cpu/math_functions.c
+++ llab/cpu/math_functions.c
@@ -1,27 +1,3 @@
-/*
-MIT License
-
-Copyright (c) 2018 Viviano Riccardo
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 #include "llab.h"
 
 void softmax(float* input, float* output, int size){
@@ -232,7 +208,7 @@ void derivative_huber_loss_array(float* y_hat, float* y,float* output, float thr
 float modified_huber_loss(float y_hat, float y, float threshold1, float threshold2){
     if(y*y_hat>= -1){
         if(threshold1-y_hat*y > 0)
-            return (threshold1-y_hat*y)*(threshold1-y_hat*y)
+            return (threshold1-y_hat*y)*(threshold1-y_hat*y);
         else
             return 0;
     }
@@ -245,17 +221,17 @@ float modified_huber_loss(float y_hat, float y, float threshold1, float threshol
 float derivative_modified_huber_loss(float y_hat, float y, float threshold1, float threshold2){
     if(y*y_hat>= -1){
         if(threshold1-y_hat*y > 0)
-            return -2*y*threshold1+2*y_hat*y*y
+            return -2*y*threshold1+2*y_hat*y*y;
         else
             return 0;
     }
     
     else{
         return -threshold2*y;
-s    }
+    }
 }
 
-void derivative_modified_huber_loss_array(float y_hat, float y, float threshold1, float threshold2 int size){
+void derivative_modified_huber_loss_array(float* y_hat, float* y, float threshold1, float* output, float threshold2, int size){
     int i;
     for(i = 0; i < size; i++){
         output[i] = derivative_modified_huber_loss(y_hat[i],y[i],threshold1,threshold2);
