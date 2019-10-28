99864c067bb40520211475009e2473ffcee7ea93
VivianoRiccardo <viviano.1706813@studenti.uniroma1.it>
2019-10-28 12:30:32 +0100

    up

--- scripts/create_library.sh
+++ scripts/create_library.sh
@@ -2,8 +2,8 @@ echo "Compiling the library"
 make
 echo "Creating the library"
 make create
-echo "Moving header files to /usr/include"
-sudo mv ../src/*.h /usr/include
+echo "Copying header files to /usr/include"
+sudo cp ../src/*.h /usr/include
 echo "Moving learning lab library to /usr/lib/gcc/x86_64-linux-gnu/5"
 sudo mv libllab.a /usr/lib/gcc/x86_64-linux-gnu/5
 
